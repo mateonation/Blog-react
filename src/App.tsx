034c25d9c8460510c8bce732314e6c745835f00b
mateonation <mattfernandezrivera@gmail.com>
2025-01-17 12:54:02 +0100

    a footer was implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 const App=()=>(
     <>
         <Header/>;
         <NavBar/>
+        <Footer/>
     </>
 )
 
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import './Footer.css';
+
+const Footer = () => (
+    <footer>
+        <p>©2025 mateonation</p>
+    </footer>
+);
+
+export default Footer;

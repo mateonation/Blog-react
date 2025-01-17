652835a0bf4d8623378baafbbdeb025cae0a8586
mateonation <mattfernandezrivera@gmail.com>
2025-01-17 12:47:30 +0100

    data path of NavBar was fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './components/Header';
-import NavBar from './components/Header/NavBar';
+import NavBar from './components/NavBar';
 
 const App=()=>(
     <>

187c6eb765ba7f07a9eec4233f09d7e820556728
mateonation <mattfernandezrivera@gmail.com>
2025-01-20 10:48:35 +0100

    new styles

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -2,8 +2,8 @@ import './ErrorPage.css';
 import { NavLink } from 'react-router-dom';
 
 const ErrorPage=()=>(
-    <div id='errorpage'>
-        <h1>ERROR 404</h1>
+    <div id='err'>
+        <h2>ERROR 404</h2>
         <p>An unexpected error occurred.</p>
         <NavLink to='/'>Click here to go back to home</NavLink>
     </div>
--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -2,8 +2,8 @@ import './NotFoundPage.css';
 import { NavLink } from 'react-router-dom';
 
 const NotFoundPage=()=>(
-    <div id='notfoundpage'>
-        <h1>ERROR 404</h1>
+    <div id='err'>
+        <h2>ERROR 404</h2>
         <p>The URL that you specified does not exist.</p>
         <NavLink to='/'>Click here to go back to home</NavLink>
     </div>

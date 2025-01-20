350aad7948c73de630388374bf94a9082a6eac09
mateonation <mattfernandezrivera@gmail.com>
2025-01-20 09:54:24 +0100

    added routes for home and an error route

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import './App.css';
 import Header from './components/Header';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const App=()=>(
-    <>
-        <Header/>
-        <NavBar/>
-        <Footer/>
-    </>
-)
+const router=createBrowserRouter([{
+    path:'/',
+    element: <h1>Home</h1>,
+},
+    {
+        path:'*',
+        element: <div>ERROR 404</div>
+    }
+]);
 
+const App=()=><RouterProvider router={router}/>
 export default App;

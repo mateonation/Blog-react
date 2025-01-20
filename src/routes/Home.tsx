0c704e18cf982ee967ad5e4f74b9227089f6c581
mateonation <mattfernandezrivera@gmail.com>
2025-01-20 10:53:54 +0100

    post interfaces created

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,12 @@
+import { LoaderFunction } from "react-router-dom";
+import { PostResponse } from "../types/app";
+
+const loader: LoaderFunction=async()=>{
+    const response=await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts:PostResponse[]=await response.json();
+    return posts.slice(0, 10);
+};
+
 const Home=()=><h2>Featured posts</h2>;
 
 export default Home;

0e4860dc5686ed9f38ea8e99622fbe81cd4cb1b7
mateonation <mattfernandezrivera@gmail.com>
2025-01-20 10:31:38 +0100

    elements of the nav bar were updated to navlink

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,19 +1,20 @@
+import { NavLink } from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar=()=>(
     <nav>
         <ul>
             <li>
-                <a href='/home'>Home</a>
+                <NavLink to='/home'>Home</NavLink>
             </li>
             <li>
-                <a href='/users'>Users</a>
+                <NavLink to='/users'>Users</NavLink>
             </li>
             <li>
-                <a href='/posts'>Posts</a>
+                <NavLink to='/posts'>Posts</NavLink>
             </li>
             <li>
-                <a href='/register'>Register</a>
+                <NavLink to='/register'>Register</NavLink>
             </li>
         </ul>
     </nav>

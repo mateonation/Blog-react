b4db6767b3fac4627d9e1640d181d133760e5c7f
mateonation <mattfernandezrivera@gmail.com>
2025-01-20 10:16:51 +0100

    navlink to '/' now does work

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -5,7 +5,7 @@ const ErrorPage=()=>(
     <div id='errorpage'>
         <h1>ERROR 404</h1>
         <p>An unexpected error occurred.</p>
-        <NavLink to='/'></NavLink>
+        <NavLink to='/'>Click here to go back to home</NavLink>
     </div>
 )
 
--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom';
 const NotFoundPage=()=>(
     <div id='notfoundpage'>
         <h1>ERROR 404</h1>
-        <p>An unexpected error occurred.</p>
-        <NavLink to='/'></NavLink>
+        <p>The URL that you specified does not exist.</p>
+        <NavLink to='/'>Click here to go back to home</NavLink>
     </div>
 )
 

c5ad32ee57ab393b5bea4f878d4903b535f28453
mateonation <mattfernandezrivera@gmail.com>
2025-01-20 10:23:29 +0100

    Home route now working

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import './App.css';
 import ErrorPage from './pages/ErrorPage';
 import NotFoundPage from './pages/NotFoundPage';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
 import RootLayout from './routes/Root';
+import Home from './routes/Home';
 
 const router=createBrowserRouter([{
     path:'/',
     element: <RootLayout />,
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage />,
+    children: [
+        {
+            index:true,
+            element: <Navigate to="/home" replace />,
+        },
+        {
+            path: 'home',
+            element: <Home />,
+        }
+    ]
 },
     {
         path:'*',

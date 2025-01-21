d4ab4143714f4befb0f64cfdf5cd684258f43736
mateonation <mattfernandezrivera@gmail.com>
2025-01-21 09:55:52 +0100

    home component cloned into a posts component

---
+++ src/routes/Posts.tsx
@@ -0,0 +1,27 @@
+import { LoaderFunction, useLoaderData } from "react-router-dom";
+import { PostResponse } from "../types/app";
+import List from "../components/List";
+
+const loader: LoaderFunction=async()=>{
+    const response=await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts:PostResponse[]=await response.json();
+    return posts.slice(0, 10);
+};
+
+const Home=()=>{
+    const posts=useLoaderData() as PostResponse[];
+    return (<>
+        <h2>Featured posts</h2>
+        {
+        posts.length===0?(
+            <p id='no-items'>No posts.</p>
+        ):(
+            <List items={posts.map(post=>({text:post.title,link:'/posts/${post.id}'}))} />
+        )
+        }
+    </>
+    );
+};
+
+Home.loader=loader;
+export default Home;

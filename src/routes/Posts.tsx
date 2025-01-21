7e6060125c45b3268efee2c002608b04ea7f8d8e
mateonation <mattfernandezrivera@gmail.com>
2025-01-21 09:59:39 +0100

    posts page now shows all the posts instead of the first 10 ones

--- src/routes/Posts.tsx
+++ src/routes/Posts.tsx
@@ -5,13 +5,13 @@ import List from "../components/List";
 const loader: LoaderFunction=async()=>{
     const response=await fetch('https://jsonplaceholder.typicode.com/posts');
     const posts:PostResponse[]=await response.json();
-    return posts.slice(0, 10);
+    return posts;
 };
 
 const Posts=()=>{
     const posts=useLoaderData() as PostResponse[];
     return (<>
-        <h2>Featured posts</h2>
+        <h2>All posts</h2>
         {
         posts.length===0?(
             <p id='no-items'>No posts.</p>

f247a574f46ff4f96144e362c497d3dbbf41865d
mateonation <mattfernandezrivera@gmail.com>
2025-01-22 08:44:23 +0100

    posts page was cloned into a details of them

---
+++ src/routes/PostDetails.tsx
@@ -0,0 +1,30 @@
+import { LoaderFunction, useLoaderData } from "react-router-dom";
+import { PostResponse } from "../types/app";
+import List from "../components/List";
+
+const loader: LoaderFunction=async()=>{
+    const response=await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts:PostResponse[]=await response.json();
+    return posts;
+};
+
+const PostsDetails=()=>{
+    const posts=useLoaderData() as PostResponse[];
+    return (<>
+        <div className="index">
+            <h2>All posts</h2>
+            <p>Total posts: <span>{posts.length}</span></p>
+        </div>
+        {
+        posts.length===0?(
+            <p id='no-items'>No posts.</p>
+        ):(
+            <List items={posts.map(post=>({text:post.title,link:'/posts/${post.id}'}))} />
+        )
+        }
+    </>
+    );
+};
+
+PostsDetails.loader=loader;
+export default PostsDetails;

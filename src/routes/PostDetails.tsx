25730db8b88b4373b08d807baf10f4185708d9dc
mateonation <mattfernandezrivera@gmail.com>
2025-01-22 10:03:26 +0100

    now it's possible to see the author of a post

--- src/routes/PostDetails.tsx
+++ src/routes/PostDetails.tsx
@@ -1,17 +1,20 @@
 import { LoaderFunction, useLoaderData } from "react-router-dom";
-import { PostResponse } from "../types/app";
+import { PostResponse, UserResponse } from "../types/app";
 
 const loader: LoaderFunction=async({params})=>{
     const responsePost=await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`);
     const post:PostResponse=await responsePost.json();
-    return {
+    const responseUser=await fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`);
+    const user:UserResponse=await responseUser.json();
+    return{
         post,
         postId:params.postId,
+        user,
     };
 };
 
 const PostDetails=()=>{
-    const {post,postId}=useLoaderData() as {post:PostResponse,postId:number;};
+    const {post,postId,user}=useLoaderData() as {post:PostResponse,postId:number,user:UserResponse;};
     if(Object.keys(post).length===0){
         return(
             <>
@@ -24,7 +27,7 @@ const PostDetails=()=>{
         return (<>
             <div className="index">
                 <h2>{post.title}</h2>
-                <p>Posted by: </p>
+                <p>Posted by: <a href={`/users/${user.id}`}>{user.name}</a></p>
             </div>
             <p>{post.body}</p>
         </>
